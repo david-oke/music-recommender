51e99a4a754173175237e3eb532b804dea05f7e0
david-oke <58450661+david-oke@users.noreply.github.com>
2022-11-12 20:41:07 -0500

    Clean up App.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 import fs from 'fs';
+import { Console } from 'console';
 
 var spotifyApi = new SpotifyWebApi({
     clientId: 'aad19679bf2e4f0f867447e17587fe1e',
     clientSecret: 'c13b5f227ebb4a609e0efeca826a099c',
     redirectUri: 'http://localhost:8888/callback'
   });
-var access_token = "BQBZFmpyI_eBjlb7C4xY8BeQ3XmS-GwbX55fpm6mBczNcWKBXadfzNQpgrf3Et90X1vS2EQKR6yBBoV1MZ9d75WvsEHHrP6OGvRKfRDcxqrExCqzSvOJp0QqUyG9KDZfpfo8HzQGGtOCBckPcAF63GZdCQOrVOik4LPiY8t_NlRC1EUtFzi5bcyw6EgclUXBjLey9Pv1M09ifCRUOnYSPO_K3lGnuKS7DPNtnsu4GCxmlDGzUzTAARTmfQf1FQCmoz_v970swuwlgs5s2oUVJqPRGBV3pA"
+var access_token = "BQCyH_k1AJXeR5eZiEHOuKF8BPtbzFO45j62HN2aarYfI_gXLkP_zbu9H1tTw70JalWRZPDEcdDtkL4MjUyS-e408DUcOIlGrGmKM0JDT3FOkhQ0P94ETr5Jmsfyhf3dW16uQqD5Az1btnruOAIQ9Z0_8BV8JG5QAYqtEcE6FdwT24zuoVcQDB75hOESYElfYDPU8Uy55cqc0Ao7ah02vO3b5tdmkbdZy85RVV9oPnY98XF5gN5U7sWVcjoZveziuY_RPdxK5uKQ0zPQ0Z-PLQ9CdzQoHA"
 spotifyApi.setAccessToken(access_token);
 
 // const API_CALL_LIMIT = 20;
 
 async function getTopTracks() {
+    let ret = []
     let savedTracks = "";
+    let savedTracksNames = []
     var data = await spotifyApi.getMyTopTracks({
         limit: 5,
         time_range: "long_term",
@@ -23,7 +26,7 @@ async function getTopTracks() {
             let trackName = tracks.at(i).name;
             let trackId = tracks.at(i).uri.slice(14);
             savedTracks += trackId + ",";
-            // savedTracks.push(trackId)
+            savedTracksNames.push(trackName);
             fs.writeFileSync('/Users/davidoke/Desktop/Programming/music-recommender/tracks.txt', JSON.stringify(trackName) + " : " + JSON.stringify(trackId) + "\n", { flag: 'a+' },err => {
                 if (err) {
                   console.error(err);
@@ -33,22 +36,29 @@ async function getTopTracks() {
       }, function(err) {
         console.log('Something went wrong!', err);
       });
-    
-      return savedTracks;
+      ret.push(savedTracks);
+      ret.push(savedTracksNames);
+      return ret;
 }
 
 async function getRecommendations(tracks) {
+    let ret = [];
     let savedTracks = "";
+    let savedTracksNames = [];
+    let savedArtistNames = [];
     var data = await spotifyApi.getRecommendations({
         seed_artists: "",
         seed_genres: "",
-        seed_tracks: tracks
+        seed_tracks: "0w0BkzzY2JgYohvF9PYnCq"
     }).then(function(data) {
         var tracks = data.body.tracks;
         for(var i = 0; i < tracks.length; i++) {
             let trackName = tracks.at(i).name;
+            let artistName = tracks.at(i).artists.at(0).name;
             let trackId = tracks.at(i).uri.slice(14);
             savedTracks += trackId + ",";
+            savedTracksNames.push(trackName);
+            savedArtistNames.push(artistName);
             fs.writeFileSync('/Users/davidoke/Desktop/Programming/music-recommender/recommendations.txt', JSON.stringify(trackName) + " : " + JSON.stringify(trackId) + "\n", { flag: 'a+' },err => {
                 if (err) {
                   console.error(err);
@@ -58,8 +68,20 @@ async function getRecommendations(tracks) {
       }, function(err) {
         console.log('Something went wrong!', err);
       });
-    return savedTracks
+    ret.push(savedTracks);
+    ret.push(savedTracksNames);
+    ret.push(savedArtistNames);
+    return ret;
 }
-var str = await getTopTracks();
-console.log(str)
-await getRecommendations(str);
+
+var topTracks = await getTopTracks();
+var recommendations = await getRecommendations(topTracks[0]);
+
+console.log("Based on your listening activity, we recommend you try: ")
+console.log("------------------------------------------------------\n")
+
+for(var i = 0; i < recommendations[1].length; i++) {
+    console.log(recommendations[1][i] + " by " + recommendations[2][i]);
+}
+
+console.log("\n------------------------------------------------------\n")
